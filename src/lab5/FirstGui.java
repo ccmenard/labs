1fbbe3bc2d28db25386a559021eace09c7573130
ccmenard <cmenard@uncc.edu>
2019-10-31 13:32:24 -0400

    finall

--- src/lab5/FirstGui.java
+++ src/lab5/FirstGui.java
@@ -6,7 +6,10 @@ package lab5;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-  
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import javax.swing.*;
 //import java.util.*; 
 
@@ -14,12 +17,13 @@ public class FirstGui extends JFrame
 {
 	private static final long serialVersionUID = 7528590832927859432L;
 	private JTextField aTextField = new JTextField();
-	private JButton doubleButton = new JButton("Calculate!");
+	private JButton doubleButton = new JButton("Does this make me a cat lady?!");
 	//private int numOfCats = 3;
 	JTextField txtInput = new JTextField("");
 	//Scanner scn = new Scanner(System.in);
 	private int inputNumofCats = Integer.parseInt(JOptionPane.showInputDialog("Enter how many cats you have: "));
 	private int inputAgeofUser = Integer.parseInt(JOptionPane.showInputDialog("Enter age in years?: "));
+	private String answer = "";
 	
 //	private Random random = new Random();
 	
@@ -27,18 +31,18 @@ public class FirstGui extends JFrame
 	{
 		public void actionPerformed(ActionEvent e)
 		{
-			
 			if(inputNumofCats < 4  || (e.getModifiers() & ActionEvent.SHIFT_MASK) !=0)
 			{
 				if(inputAgeofUser < 25 || (e.getModifiers() & ActionEvent.SHIFT_MASK) !=0)
 				{
-					updateTextField("You are not a cat lady!");
-					//String answer = "You are not a cat lady!"
+					String answer = "You are not a cat lady!";
+					updateTextField(answer);
 				}
 			}
 			else
 			{
-			updateTextField("LOL You are a cat lady!");
+			String answer = "LOL You are a cat lady!";
+			updateTextField(answer);
 			
 			}
 		} 
@@ -46,7 +50,7 @@ public class FirstGui extends JFrame
 	private void updateTextField(String answer)
 	{
 		
-		aTextField.setText(answer);
+		aTextField.setText("You have " + inputNumofCats + " cats. And are " + inputAgeofUser + " years old." + "\n" + answer);
 		validate();
 	}
 	public FirstGui() 
@@ -62,10 +66,64 @@ public class FirstGui extends JFrame
 		getContentPane().add(doubleButton, BorderLayout.SOUTH);
 		doubleButton.addActionListener(new DoubleActionListener());
 		getContentPane().add(aTextField, BorderLayout.CENTER);
-		//aTextField.setText("How many cats do you have?: " + inputNumofCats);
 		aTextField.setText("You have " + inputNumofCats + " cats. And are " + inputAgeofUser + " years old.");
+		setJMenuBar(getMyMenuBar());
+		updateTextField(this.answer);
+		
 	}
-	
+	private JMenuBar getMyMenuBar()
+	{
+		JMenuBar jmenuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic('F');
+		jmenuBar.add(fileMenu);
+		
+		JMenuItem saveItem = new JMenuItem("Save");
+		fileMenu.add(saveItem);
+		saveItem.setMnemonic('S');
+		
+		saveItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				saveToFile();
+			}
+		});
+		
+		JMenuItem openItem = new JMenuItem("Open");
+		openItem.setMnemonic('O');
+		fileMenu.add(openItem);
+		return jmenuBar;
+	}
+	private void saveToFile()
+	{
+		JFileChooser jfc = new JFileChooser();
+		if (jfc.showSaveDialog(this) != JFileChooser.APPROVE_OPTION)
+			return;
+		if(jfc.getSelectedFile() == null)
+				return; 
+		File chosenFile = jfc.getSelectedFile();
+		if (jfc.getSelectedFile().exists())
+		{
+			String message = "File " + jfc.getSelectedFile().getName() + " exists. Overwrite?";
+			if (JOptionPane.showConfirmDialog(this, message) != JOptionPane.YES_OPTION)
+				return;
+		}
+		try 
+		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(chosenFile));
+			writer.write("You have " + inputNumofCats + " cats. And are " + inputAgeofUser + " years old. " + this.answer);
+			writer.flush();
+			writer.close();
+			
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Could not write file", JOptionPane.ERROR_MESSAGE);
+		}
+		
+	}
 	public static void main(String[] args)
 	{
 		new FirstGui();
