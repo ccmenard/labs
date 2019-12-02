5d570137f8d9677a6e1d77313935e3a564838c76
ccmenard <cmenard@uncc.edu>
2019-12-02 14:00:49 -0500

    update

--- src/lab6/AAQuizGUI.java
+++ src/lab6/AAQuizGUI.java
@@ -1,86 +1,110 @@
 package lab6;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
-import java.awt.BorderLayout;
+/*import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Random;
-
+*/
 import javax.swing.*;
 
 public class AAQuizGUI extends JFrame
 {
-	public String[] SHORT_NAMES = {"A","R","N","D","C","Q","E","G","H",
+	public static String[] SHORT_NAMES = {"A","R","N","D","C","Q","E","G","H",
 			"I","L","K","M","F","P","S","T","W","Y","V"};
-	public String[] FULL_NAMES = {"alanine","arginine","asparagine","aspartic acid","cysteine",
+	public static String[] FULL_NAMES = {"alanine","arginine","asparagine","aspartic acid","cysteine",
 			"glutamine","glutamic acid","glycine","histidine","isoleucine","leucine","lysine","methionine",
 			"phenylalanine","proline","serine","threonine","tryptophan","tyrosine","valine"} ;
-	Random random = new Random();
-	int randomnumber = random.nextInt(FULL_NAMES.length);
 	private static final long serialVersionUID = 7528590832927859569L;
 	private JTextField aTextField = new JTextField();
+	private JTextField textField = new JTextField();
 	private JButton doubleButton2 = new JButton("End Quiz");
-	private JButton doubleButton1 = new JButton("Start Quiz");
-	private String questionFullAA = FULL_NAMES[randomnumber];
-	private String questionShortAA = SHORT_NAMES[randomnumber];
-	private String inputAAanswer = String.valueOf(JOptionPane.showInputDialog("Enter one letter uppercase abrrevation for " + questionFullAA));
+	private JButton doubleButton1 = new JButton("Generate Question");
+	
 	int numCorrect = 0;
 	int numWrong = 0;
-	int total = numCorrect + numWrong;
-	private String response = ""; 
-	boolean boo = true;
-	
-	
 	private class DoubleActionListener implements ActionListener
 	{
+		
 		public void actionPerformed(ActionEvent e)
 		{
-			if(inputAAanswer == questionShortAA || (e.getModifiers() & ActionEvent.SHIFT_MASK) !=0)
+			Random random = new Random();
+			int randomnumber = random.nextInt(FULL_NAMES.length);
+			String questionFullAA = FULL_NAMES[randomnumber];
+			String questionShortAA = SHORT_NAMES[randomnumber];
+			String inputAAanswer = JOptionPane.showInputDialog("Enter one letter uppercase abrrevation for " + questionFullAA);
+			aTextField.setEditable(false);
+			
+			if(inputAAanswer.toUpperCase().contentEquals(questionShortAA))
 			{
-				String response = "Correct!";
-				updateTextField(response);
+				
 				numCorrect++;
 				
 				
 			}
 			else
 			{
-				String response = "Wrong!";
-				updateTextField(response);
+				
 				numWrong++;
 				
 			}
+			updateTextField(numCorrect,numWrong);
 			
 		}
+			
 	}
-		private void updateTextField(String response)
+		private class CloseListener implements ActionListener
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		}
+		private void updateTextField(int numCorrect, int numWrong)
 		{
 			
-			aTextField.setText("You entered " + inputAAanswer + ". The correct answer is :" + questionShortAA + ".");
+			int total = numCorrect + numWrong;
+			textField.setText(numCorrect + " correct out of :" + total);
 			validate();
-			
-			JOptionPane.showMessageDialog(null, numCorrect + "Correct out of " + total);
 		}
+		private JPanel getNewPanel()
+		{
+			JPanel newPanel = new JPanel();
+			newPanel.setLayout(new GridLayout(1,2));
+			newPanel.add(doubleButton1);
+			newPanel.add(doubleButton2);
+			doubleButton1.addActionListener(new DoubleActionListener());
+			return newPanel;
+			
+		}
+		
+		
+		boolean boo = true;
 		public AAQuizGUI() 
 		{
 			super("Claire's Amino Acid Quiz");
-			setSize(300,300);
+			setSize(500,500);
 			setLocationRelativeTo(null);
 			setVisible(true);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			getContentPane().setLayout(new BorderLayout());
 			getContentPane().add(doubleButton1, BorderLayout.EAST);
 			getContentPane().add(doubleButton2, BorderLayout.WEST);
-			doubleButton1.addActionListener(new DoubleActionListener());
-			//doubleButton2.close;
-			getContentPane().add(aTextField, BorderLayout.CENTER);
-			updateTextField(this.response);
+			doubleButton2.addActionListener(new CloseListener());
+			getContentPane().add(textField, BorderLayout.CENTER);
+			getContentPane().add(getNewPanel(), BorderLayout.SOUTH);
+			aTextField.setEditable(false);
+			textField.setEditable(false);
+			
 			
 		}
 		
